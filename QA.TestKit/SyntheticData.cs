27d827b9df600a1438ab0079c9eb072ef86c5928
moshin34 <145878819+moshin34@users.noreply.github.com>
2025-08-13 19:32:23 -0400

    Update SyntheticData.cs

--- QA.TestKit/SyntheticData.cs
+++ QA.TestKit/SyntheticData.cs
@@ -1,10 +1,10 @@
+// QA.TestKit/SyntheticData.cs
 using System;
-using System.Collections.Generic;
 
 namespace NT8.SDK.QA.TestKit
 {
     /// <summary>
-    /// Generates small synthetic bar streams for testing.
+    /// Generates small synthetic bar streams for testing (no 'yield' usage).
     /// </summary>
     public static class SyntheticData
     {
@@ -16,78 +16,99 @@ namespace NT8.SDK.QA.TestKit
         /// <param name="bars">Number of bars to generate.</param>
         /// <param name="startPrice">Starting price.</param>
         /// <param name="step">Price step per bar.</param>
-        /// <returns>Enumerable bar stream.</returns>
-        public static IEnumerable<Bar> TrendingUp(string symbol, DateTime startEt, int bars, decimal startPrice, decimal step)
+        /// <returns>Array of bars.</returns>
+        public static Bar[] TrendingUp(string symbol, DateTime startEt, int bars, decimal startPrice, decimal step)
         {
+            if (bars < 0) bars = 0;
+            var result = new Bar[bars];
             decimal price = startPrice;
+
             for (int i = 0; i < bars; i++)
             {
                 decimal open = price;
                 decimal close = price + step;
                 decimal high = open > close ? open : close;
                 decimal low = open < close ? open : close;
+
                 Bar bar = new Bar();
                 bar.Et = startEt.AddMinutes(i);
                 bar.Open = open;
                 bar.High = high;
                 bar.Low = low;
                 bar.Close = close;
-                yield return bar;
+
+                result[i] = bar;
                 price = close;
             }
+
+            return result;
         }
 
         /// <summary>
         /// Generates a downward trending series of bars.
         /// </summary>
-        public static IEnumerable<Bar> TrendingDown(string symbol, DateTime startEt, int bars, decimal startPrice, decimal step)
+        public static Bar[] TrendingDown(string symbol, DateTime startEt, int bars, decimal startPrice, decimal step)
         {
+            if (bars < 0) bars = 0;
+            var result = new Bar[bars];
             decimal price = startPrice;
+
             for (int i = 0; i < bars; i++)
             {
                 decimal open = price;
                 decimal close = price - step;
                 decimal high = open > close ? open : close;
                 decimal low = open < close ? open : close;
+
                 Bar bar = new Bar();
                 bar.Et = startEt.AddMinutes(i);
                 bar.Open = open;
                 bar.High = high;
                 bar.Low = low;
                 bar.Close = close;
-                yield return bar;
+
+                result[i] = bar;
                 price = close;
             }
+
+            return result;
         }
 
         /// <summary>
         /// Generates a choppy bar sequence alternating direction.
         /// </summary>
-        public static IEnumerable<Bar> Choppy(string symbol, DateTime startEt, int bars, decimal startPrice, decimal step)
+        public static Bar[] Choppy(string symbol, DateTime startEt, int bars, decimal startPrice, decimal step)
         {
+            if (bars < 0) bars = 0;
+            var result = new Bar[bars];
             decimal price = startPrice;
             bool up = true;
+
             for (int i = 0; i < bars; i++)
             {
                 decimal open = price;
                 decimal close = up ? price + step : price - step;
                 decimal high = open > close ? open : close;
                 decimal low = open < close ? open : close;
+
                 Bar bar = new Bar();
                 bar.Et = startEt.AddMinutes(i);
                 bar.Open = open;
                 bar.High = high;
                 bar.Low = low;
                 bar.Close = close;
-                yield return bar;
+
+                result[i] = bar;
                 price = close;
                 up = !up;
             }
+
+            return result;
         }
     }
 
     /// <summary>
-    /// Plain bar record used by synthetic data generators.
+    /// Plain bar struct used by synthetic data generators.
     /// </summary>
     public struct Bar
     {
