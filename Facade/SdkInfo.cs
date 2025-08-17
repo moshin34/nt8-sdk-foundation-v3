0e268fa21798da72228627de69e887c4d2031ff3
moshin34 <145878819+moshin34@users.noreply.github.com>
2025-08-17 02:49:35 -0400

    chore: add SDK info placeholder

---
+++ Facade/SdkInfo.cs
@@ -0,0 +1,13 @@
+namespace NT8.SDK.Facade
+{
+    /// <summary>
+    /// Lightweight SDK info placeholder to satisfy CI and provide version identity.
+    /// </summary>
+    public static class SdkInfo
+    {
+        /// <summary>
+        /// Gets the SDK version string.
+        /// </summary>
+        public static string Version => "0.1.0";
+    }
+}
